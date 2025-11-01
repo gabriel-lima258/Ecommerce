7dc0def3a570298aae738692c64ba03e7ba4589d
Gabriel Lima <gabriel58221@gmail.com>
2025-11-01 09:20:00 -0300

    Created delete response error

---
+++ src/main/java/com/gtech/Ecommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.gtech.Ecommerce.controllers.handlers;
+
+import com.gtech.Ecommerce.dto.CustomError;
+import com.gtech.Ecommerce.services.exceptions.DatabaseException;
+import com.gtech.Ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+// classe personalizada para lidar com erro em controllers
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/gtech/Ecommerce/services/ProductService.java
+++ src/main/java/com/gtech/Ecommerce/services/ProductService.java
@@ -3,10 +3,15 @@ package com.gtech.Ecommerce.services;
 import com.gtech.Ecommerce.dto.ProductDTO;
 import com.gtech.Ecommerce.entities.Product;
 import com.gtech.Ecommerce.repositories.ProductRepository;
+import com.gtech.Ecommerce.services.exceptions.DatabaseException;
+import com.gtech.Ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -18,7 +23,9 @@ public class ProductService {
     // services devolvem DTO ao inves da propria entidade
     @Transactional(readOnly = true) // aumenta a perfomance de leitura e bloquia o write
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado")
+        ); // joga excessao tratada
         return new ProductDTO(product);
     }
 
@@ -40,25 +47,38 @@ public class ProductService {
     }
 
     @Transactional
-    public ProductDTO update(Long id, ProductDTO productDTO) {
-        // referencia o id product sem abrir o banco
-        Product product = repository.getReferenceById(id);
-        copyDtoToEntity(productDTO, product);
-        product = repository.save(product);
+    public ProductDTO update(Long id, ProductDTO dto) {
+        try {
+            // referencia o id product sem abrir o banco
+            Product product = repository.getReferenceById(id);
+            copyDtoToEntity(dto, product);
+            product = repository.save(product);
 
-        return new ProductDTO(product);
+            return new ProductDTO(product);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
     }
 
-    @Transactional
+    // metodo usado quando há conflito entre entidades many to many
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 
-    private void copyDtoToEntity(ProductDTO productDTO, Product product) {
-        product.setName(productDTO.getName());
-        product.setDescription(productDTO.getDescription());
-        product.setPrice(productDTO.getPrice());
-        product.setImgUrl(productDTO.getImgUrl());
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
     }
 
 }
