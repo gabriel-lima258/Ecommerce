ac8516a8e20a9e23b59cb4f98c98fd481da9364d
Gabriel Lima <gabriel58221@gmail.com>
2025-11-09 19:22:49 -0300

    added user details service

---
+++ src/main/java/com/gtech/Ecommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.gtech.Ecommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
